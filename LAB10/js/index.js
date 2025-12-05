51ff8897f812b4a236b9241e220cf8100a2260f9
MH276-lll <mhdsayad909@gmail.com>
2025-12-05 01:40:32 +0000

    LAB10

--- LAB10/js/index.js
+++ LAB10/js/index.js
@@ -4,7 +4,7 @@ const contacts = []; // Array to store contacts
 
 // Function to display contacts
 function displayContacts() {
-    contactsContainer.innerHTML=''; // Clear existing contacts
+    contactsContainer.innerHTML = ''; // Clear existing contacts
 
     // Iterate over contacts
     for(let index = 0; index < contacts.length; index++) {
@@ -15,21 +15,22 @@ function displayContacts() {
 
         let textAsArray = [
             contact.firstName,
-            contact, lastName,
-            contact, phone,
+            contact.lastName,
+            contact.phone,
             contact.email,
+
             '<button class="btn btn-danger btn-sm" onclick="deleteContact(' + index + ')">Delete</button>'
-    ];
+        ];
 
         // Inner loop used to add contact information to each row.
-        for(let innerIndex; innerIndex < textAsArray.length; innerIndex++) {
-        let span = document.createElement('span');
-        span.innerHTML = textAsArray[innerIndex];
+        for(let innerIndex = 0; innerIndex < textAsArray.length; innerIndex++) {
+            let span = document.createElement('span');
+            span.innerHTML = textAsArray[innerIndex];
 
-        li.appendChild(span);
-    }
+            li.appendChild(span);
+        }
 
-    contactsContainer.appendChild(li);
+        contactsContainer.appendChild(li);
     }
 }
 
@@ -47,10 +48,10 @@ contactForm.addEventListener('submit', function (e) {
     const firstName = document.getElementById('firstName').value;
     const lastName = document.getElementById('lastName').value;
     const email = document.getElementById('email').value;
-    const  phone = document.getElementById('phone').value;
+    const phone = document.getElementById('phone').value;
 
     // Add contact to the array. The below will create an array of objects.
-    contacts.push({ firstName, lastName, email, phone});
+    contacts.push({ firstName, lastName, email, phone });
     displayContacts();
     contactForm.reset(); // Clear the form
 });
